bd7245af73f93fe735a47758cf393af6dbae9f41
mahmoud <mahmoud@Mahmoud>
2017-02-07 04:11:38 -0600

    Merge origin/master
    Conflicts:
    	nbproject/ant-deploy.xml
    	nbproject/project.properties

--- web/resources/js/system_init.js
+++ web/resources/js/system_init.js
@@ -3,11 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-    coreUrl: "http://" + window.location.host + "/GoHost/api/",
+function LoginHandler() {
+    $.getJSON('http://' + window.location.host + '/GoHost/api/'+'user/login?name='+$('#loginUser').val()+'&password='+$('#loginPass').val()).done(loginCallBack);
+}
+
+function loginCallBack(Id) {
+	if(Id != 0){
+		// Storing the id number of the user
+		sessionStorage.setItem('id',Id);
+		// Store the username for future use
+		sessionStorage.setItem('username', $('#loginUser').val());
+		// If the account exists, open a new page
+		window.location.href = 'Home.html#' + Id;
+	}
+	else {
+		// If the account doesn't exist, show a text field saying that the account entered does not exist
+		$('#loginUser').val('Your desired username is unfortunately already in use...');
+		$('#test').show();
+	}
+}
+
+coreUrl: "http://" + window.location.host + "/GoHost/api/";
+
+        
 sys_init = {
     createUser: function(email, password) {
         url = coreUrl + "user"
-                $.ajax({
+        $.ajax({
           url: url,
           type: 'post',
           data: toSend, 
