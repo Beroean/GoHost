0cff15dd7eec163d19dba12beb214a6abf9556e2
jerbaer <jerry95wang@gmail.com>
2017-02-07 06:52:12 -0600

    Merge origin/master
    Conflicts:
    	web/resources/js/system_init.js

--- web/resources/js/EventList.js
+++ web/resources/js/EventList.js
@@ -4,26 +4,26 @@
  * and open the template in the editor.
  */
 
-eventsList={
-events:null, //array of events contained in the list
-accessor: null, //user that is accessing the list
-viewType: 0, // 0 means view events hosting, 1 means events attending, 2 means all visible events
+eventsList = {
+    events: null, //array of events contained in the list
+    accessor: null, //user that is accessing the list
+    viewType: 0, // 0 means view events hosting, 1 means events attending, 2 means all visible events
 
-create: function(accessor, viewType){
-    //get events into the list based on viewtype and accessor, then create events using idevent/populate using later methods
-    
-},
-getEventsHosting: function(){
- //get all events accessor is hosting, put them in events (0)   
-},
-getEventsAttending: function(){
-    //self explanatory (1)
-},
-getEventsVisible: function(){
-  //self explantory (2)  
-},
-getEventsList: function(){
-    return events;
-},
+    create: function (accessor, viewType) {
+        //get events into the list based on viewtype and accessor, then create events using idevent/populate using later methods
+
+    },
+    getEventsHosting: function () {
+        //get all events accessor is hosting, put them in events (0)   
+    },
+    getEventsAttending: function () {
+        //self explanatory (1)
+    },
+    getEventsVisible: function () {
+        //self explantory (2)  
+    },
+    getEventsList: function () {
+        return events;
+    },
 
 }
