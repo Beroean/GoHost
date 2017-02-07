8a33d4f57e278e0f4296ee2bddec01b66ffd5971
mahmoud <mahmoud@Mahmoud>
2017-02-07 05:05:56 -0600

    Merge origin/master
    Conflicts:
    	nbproject/ant-deploy.xml
    	nbproject/project.properties

--- web/resources/js/system_init.js
+++ web/resources/js/system_init.js
@@ -2,37 +2,11 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
-
-function LoginHandler() {
-    $.getJSON('http://' + window.location.host + '/GoHost/api/'+'user/login?name='+$('#loginUser').val()+'&password='+$('#loginPass').val()).done(loginCallBack);
-}
-
-function loginCallBack(Id) {
-	if(Id != 0){
-		// Storing the id number of the user
-		sessionStorage.setItem('id',Id);
-		// Store the username for future use
-		sessionStorage.setItem('username', $('#loginUser').val());
-		// If the account exists, open a new page
-		window.location.href = 'Home.html#' + Id;
-	}
-	else {
-		// If the account doesn't exist, show a text field saying that the account entered does not exist
-		$('#loginUser').val('Your desired username is unfortunately already in use...');
-		$('#test').show();
-	}
-}
-
-
-
-        
+ */     
 sys_init = {
     coreUrl: "http://" + window.location.host + "/GoHost/api/",
-    
-    
     doLogin: function() {
-        var url = sys_init.coreUrl + "user?email="+$('#email').val()+"&password="+$('#password').val();
+        var url = sys_init.coreUrl + "user?email="+$('#loginEmail').val()+"&password="+$('#loginPass').val();
         $.getJSON(url).done(sys_init.moveToHome);
     },
     
@@ -47,11 +21,20 @@ sys_init = {
           data: toSend, 
           contentType: 'application/json',
           dataType: 'json',
-          success: sys_init.moveToHome
+          success: /*hide this div and show the log in div*/
         });
     },
     moveToHome: function() {
-        //Take it home!
+        if(Id != 0){
+            // Storing the id number of the user
+            sessionStorage.setItem('id',Id);
+            //Take them to home page
+        } else {
+            $('#regPassWarning').show();
+        }
+    },
+    setUpIndex: function() {
+        //This sets up the index.html page complete with hiding certain divs
+        //shit
     }
 }
-
